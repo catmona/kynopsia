8dfbfa52e7f7962f37b293fc5d1a268bffbc3354
Catherine Sangiovanni <catherine.sangiovanni@gmail.com>
2024-04-05 16:35:56 -0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,100 @@
-import { Switch } from '@headlessui/react'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
+    const [input, setInput] = useState('')
+    const [history, setHistory] = useState<string[]>([])
+    const [historyIndex, setHistoryIndex] = useState(-1)
+
+    const msgContainer = useRef<HTMLDivElement>(null)
+
+    function pushHistory(text: string) {
+        const buffer = [...history]
+        const existingIndex = buffer.indexOf(text)
+
+        if (existingIndex != -1) {
+            buffer.splice(existingIndex, 1)
+        }
+
+        while (buffer.length >= 10) {
+            buffer.splice(buffer.length, 1)
+        }
+
+        setHistory([text, ...buffer])
+    }
+
+    function submitInput(e: SubmitEvent) {
+        e.preventDefault()
+
+        appendMessage(input)
+        pushHistory(input)
+
+        setInput('')
+        setHistoryIndex(-1)
+    }
+
+    function appendMessage(text: string) {
+        const ele = document.createElement('p')
+        ele.textContent = text
+        msgContainer.current?.appendChild(ele)
+    }
+
+    function handleHistory(e: KeyboardEvent) {
+        switch (e.key) {
+            case 'ArrowUp': {
+                e.preventDefault()
+
+                historyIndex < history.length - 1
+                    ? setHistoryIndex(historyIndex + 1)
+                    : {}
+                break
+            }
+            case 'ArrowDown': {
+                e.preventDefault()
+
+                historyIndex >= history.length - 1
+                    ? setHistoryIndex(historyIndex - 1)
+                    : {}
+                historyIndex == 0 ? setInput('') : {}
+                break
+            }
+        }
+    }
+
+    useEffect(() => {
+        historyIndex != -1 ? setInput(history[historyIndex]) : {}
+    }, [historyIndex])
+
     return (
         <>
-            <h1 className="font-semibold">test</h1>
-            <Switch className="inline-block h-6 w-11 bg-black transition" />
+            <div className="h-screen w-screen bg-black flex">
+                <div className="flex justify-center items-center grow">
+                    <div className="w-[50%] h-[50%] flex bg-slate-800 flex-col">
+                        <div
+                            className="grow p-4 text-white font-mono"
+                            ref={msgContainer}
+                        >
+                            <p>out</p>
+                            <p>out</p>
+                            <p>out</p>
+                            <p>out</p>
+                            <p>out</p>
+                        </div>
+                        <form onSubmit={submitInput}>
+                            <input
+                                type="text"
+                                autoFocus
+                                value={input}
+                                onKeyDown={(e) => handleHistory(e)}
+                                onChange={(text) => {
+                                    setHistoryIndex(-1)
+                                    setInput(text.currentTarget.value)
+                                }}
+                                className="w-full focus:outline-none p-2 text-white/80 bg-transparent"
+                            />
+                        </form>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
